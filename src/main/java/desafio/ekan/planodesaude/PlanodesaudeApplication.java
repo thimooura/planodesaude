72d3c872b91eee7f1e2a069cfa86661f42d34ec3
thimooura <t.695@hotmail.com>
2024-08-07 10:30:43 -0300

    init

---
+++ src/main/java/desafio/ekan/planodesaude/PlanodesaudeApplication.java
@@ -0,0 +1,13 @@
+package desafio.ekan.planodesaude;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanodesaudeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanodesaudeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/desafio/ekan/planodesaude/PlanodesaudeApplicationTests.java
@@ -0,0 +1,13 @@
+package desafio.ekan.planodesaude;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanodesaudeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
